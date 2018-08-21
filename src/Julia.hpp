e70171a275259f1d9c46e4d3d0e1dd749eeef298
Kim <kim.schmider@hotmail.com>
2018-08-21 15:28:16 +0200

    Encapsulate value and add constructors

--- src/Julia.hpp
+++ src/Julia.hpp
@@ -4,12 +4,19 @@
 #include <array>
 #include <julia/julia.h>
 
-struct JuliaValue
+class JuliaValue
 {
+public:
+    JuliaValue(jl_value_t* boxed_value_) : _boxed_value{boxed_value_} {}
+
+    JuliaValue() = default;
+    JuliaValue(const JuliaValue&) = default;
+    JuliaValue(JuliaValue&&) = default;
+
     template<typename TargT>
     TargT get()
     {
-        return impl::unbox<TargT>(boxed_value);
+        return impl::unbox<TargT>(_boxed_value);
     }
 
     template<typename TargT>
@@ -18,12 +25,13 @@ struct JuliaValue
         return get<TargT>();
     }
 
-    jl_value_t* boxed_value;
+private:
+    jl_value_t* _boxed_value;
 };
 
 JuliaValue jl(const char* src_str_)
 {
-    return {jl_eval_string(src_str_)};
+    return jl_eval_string(src_str_);
 }
 
 template<typename... ArgTs>
@@ -32,5 +40,5 @@ JuliaValue jl(const char* fn_name_, ArgTs... args_)
     std::array<jl_value_t*, sizeof...(args_)> boxed_args{impl::box(args_)...};
 
     jl_value_t* func = jl_eval_string(fn_name_);
-    return {jl_call(func, boxed_args.data(), boxed_args.size())};
+    return jl_call(func, boxed_args.data(), boxed_args.size());
 }
