5aaeca7530c3890e0ee93a51eb30b645edaa03fd
Kim <kim.schmider@hotmail.com>
2018-08-22 22:17:35 +0200

    Add base error type

--- src/Errors.hpp
+++ src/Errors.hpp
@@ -5,21 +5,29 @@
 namespace jl
 {
 
-struct language_error : std::runtime_error
+struct error : std::runtime_error
 {
     using std::runtime_error::runtime_error;
+    virtual ~error() = 0;
+};
+
+error::~error() {}
+
+struct language_error : error
+{
+    using error::error;
     virtual ~language_error() {}
 };
 
-struct value_error : std::runtime_error
+struct value_error : error
 {
-    using std::runtime_error::runtime_error;
+    using error::error;
     virtual ~value_error() {}
 };
 
-struct load_error : std::runtime_error
+struct load_error : error
 {
-    using std::runtime_error::runtime_error;
+    using error::error;
     virtual ~load_error() {}
 };
 
