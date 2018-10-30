a6cfeb2d6e65b0049df9e7be6aa3836f2cd53eea
Kim <login@schmider.kim>
2018-10-30 20:58:35 +0100

    Add safety assertions to syncing (assert sizes match and not yet synced)

--- src/Sync.hpp
+++ src/Sync.hpp
@@ -56,6 +56,7 @@ struct type
 template<typename... TypeTs>
 void sync(TypeTs&&... ts_)
 {
+    assert(impl::num_synced_types == 0 && "You may sync types only once");
     impl::num_synced_types = sizeof...(ts_);
     impl::synced_cpp_types = new std::type_index[sizeof...(ts_)]{
         typeid(typename TypeTs::cpp_type)...};
@@ -63,6 +64,16 @@ void sync(TypeTs&&... ts_)
     impl::synced_jl_types = new jl_datatype_t* [sizeof...(ts_)] {
         reinterpret_cast<jl_datatype_t*>(jl_eval_string(ts_.julia_type))...
     };
+
+    jl_function_t* sizeof_func{jl_eval_string("sizeof")};
+    JL_GC_PUSH1(sizeof_func);
+
+    assert("Synced datatype sizes do not match"
+           && ((sizeof(typename TypeTs::cpp_type)
+                == static_cast<std::size_t>(jl_unbox_int64(
+                       jl_call1(sizeof_func, jl_eval_string(ts_.julia_type)))))
+               && ...));
+    JL_GC_POP();
 }
 
 } // namespace jl
