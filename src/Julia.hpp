1d03f662c54a4bbc29a8a5c6145ab28145af0f9a
Kim <login@schmider.kim>
2018-10-31 10:39:52 +0100

    Split Array & Value into their own files

--- src/Julia.hpp
+++ src/Julia.hpp
@@ -5,6 +5,8 @@
 #include "GenericString.hpp"
 #include "GlobalInstance.hpp"
 #include "Helpers.hpp"
+#include "Literals.hpp"
+#include "Value.hpp"
 
 #include <array>
 #include <fstream>
@@ -15,126 +17,6 @@
 
 namespace jl
 {
-
-class value;
-
-template<typename ElemT>
-class array
-{
-public:
-    template<typename... ElemTs,
-             typename CommT = std::common_type_t<ElemTs...>,
-             typename std::enable_if_t<std::is_same_v<CommT, ElemT>>* = nullptr>
-    array(ElemTs... elems_) noexcept
-        : array{impl::get_type<CommT>(), sizeof...(ElemTs)}
-    {
-        impl::make_arg_vec<ElemT, ElemTs...>::make(_arr, 0, elems_...);
-    }
-
-    array(jl_datatype_t* type_, std::size_t size_) noexcept : _size{size_}
-    {
-        jl_value_t* array_type{jl_apply_array_type(
-            reinterpret_cast<jl_value_t*>(type_), dimensions)};
-        _arr = static_cast<ElemT*>(std::malloc(_size * sizeof(ElemT)));
-        _metadata = jl_ptr_to_array_1d(array_type, _arr, _size, true);
-    }
-
-    array(jl_array_t* metadata_) noexcept
-        : _arr{static_cast<ElemT*>(jl_array_data(metadata_))}
-        , _metadata{metadata_}
-        , _size{jl_array_len(metadata_)}
-    {
-    }
-
-    array(const array&) noexcept = default;
-    array(array&&) noexcept = default;
-
-    std::size_t size() const noexcept { return _size; }
-    bool empty() const noexcept { return _size > 0; }
-    ElemT* data() noexcept { return _arr; }
-    jl_array_t* get_boxed_data() noexcept { return _metadata; }
-    ElemT* begin() noexcept { return data(); }
-    ElemT* end() noexcept { return data() + _size; }
-    const ElemT* cbegin() const noexcept { return data(); }
-    const ElemT* cend() const noexcept { return data() + _size; }
-    ElemT operator[](std::size_t index_) { return data()[index_]; }
-    const ElemT operator[](std::size_t index_) const { return data()[index_]; }
-    ElemT front() { return *data(); }
-    ElemT back() { return data()[_size - 1]; }
-    const ElemT front() const { return *data(); }
-    const ElemT back() const { return data()[_size - 1]; }
-
-private:
-    static constexpr std::size_t dimensions{1};
-    ElemT* _arr;
-    jl_array_t* _metadata;
-    const std::size_t _size;
-};
-
-class value
-{
-public:
-    value(jl_value_t* boxed_value_) noexcept : _boxed_value{boxed_value_} {}
-
-    value() noexcept = default;
-
-    template<typename T>
-    value(T&& obj_) : _boxed_value{box(obj_)}
-    {
-    }
-
-    template<typename TargT,
-             std::enable_if_t<std::is_fundamental<TargT>{}
-                              || impl::is_array<TargT>{}>* = nullptr>
-    TargT get()
-    {
-        if constexpr (std::is_integral_v<TargT>)
-            return static_cast<TargT>(impl::unbox<long>(_boxed_value));
-        else if constexpr (std::is_floating_point_v<TargT>)
-            return static_cast<TargT>(impl::unbox<double>(_boxed_value));
-        else
-            return *this;
-    }
-
-    template<typename TargT,
-             std::enable_if_t<!std::is_fundamental<TargT>{}>* = nullptr>
-    TargT get() noexcept
-    {
-        if constexpr (std::is_same_v<TargT, jl_value_t*>)
-            return static_cast<jl_value_t*>(*this);
-        else if constexpr (std::is_pointer_v<TargT>)
-            return reinterpret_cast<TargT>(_boxed_value);
-        else
-            return *reinterpret_cast<std::decay_t<TargT>*>(_boxed_value);
-    }
-
-    template<
-        typename TargT,
-        typename = std::enable_if_t<std::is_fundamental<TargT>{}
-                                    && !std::is_same_v<TargT, jl_value_t*>>>
-    operator TargT()
-    {
-        return impl::unbox<TargT>(_boxed_value);
-    }
-
-    template<typename TargT,
-             std::enable_if_t<std::is_class_v<std::decay_t<TargT>>>* = nullptr>
-    operator TargT()
-    {
-        return get<TargT>();
-    }
-
-    operator jl_value_t*() { return _boxed_value; }
-
-    template<typename ElemT>
-    explicit operator array<ElemT>() noexcept
-    {
-        return reinterpret_cast<jl_array_t*>(_boxed_value);
-    }
-
-private:
-    jl_value_t* _boxed_value;
-};
 
 template<typename ValT, typename... ArgTs>
 value make_value(ArgTs&&... args_)
---
+++ src/Value.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "Array.hpp"
+
+namespace jl
+{
+
+class value
+{
+public:
+    value(jl_value_t* boxed_value_) noexcept : _boxed_value{boxed_value_} {}
+
+    value() noexcept = default;
+
+    template<typename T>
+    value(T&& obj_) : _boxed_value{box(obj_)}
+    {
+    }
+
+    template<typename TargT,
+             std::enable_if_t<std::is_fundamental<TargT>{}
+                              || impl::is_array<TargT>{}>* = nullptr>
+    TargT get()
+    {
+        if constexpr (std::is_integral_v<TargT>)
+            return static_cast<TargT>(impl::unbox<long>(_boxed_value));
+        else if constexpr (std::is_floating_point_v<TargT>)
+            return static_cast<TargT>(impl::unbox<double>(_boxed_value));
+        else
+            return *this;
+    }
+
+    template<typename TargT,
+             std::enable_if_t<!std::is_fundamental<TargT>{}>* = nullptr>
+    TargT get() noexcept
+    {
+        if constexpr (std::is_same_v<TargT, jl_value_t*>)
+            return static_cast<jl_value_t*>(*this);
+        else if constexpr (std::is_pointer_v<TargT>)
+            return reinterpret_cast<TargT>(_boxed_value);
+        else
+            return *reinterpret_cast<std::decay_t<TargT>*>(_boxed_value);
+    }
+
+    template<
+        typename TargT,
+        typename = std::enable_if_t<std::is_fundamental<TargT>{}
+                                    && !std::is_same_v<TargT, jl_value_t*>>>
+    operator TargT()
+    {
+        return impl::unbox<TargT>(_boxed_value);
+    }
+
+    template<typename TargT,
+             std::enable_if_t<std::is_class_v<std::decay_t<TargT>>>* = nullptr>
+    operator TargT()
+    {
+        return get<TargT>();
+    }
+
+    operator jl_value_t*() { return _boxed_value; }
+
+    template<typename ElemT>
+    explicit operator array<ElemT>() noexcept
+    {
+        return reinterpret_cast<jl_array_t*>(_boxed_value);
+    }
+
+private:
+    jl_value_t* _boxed_value;
+};
+
+} // namespace jl
