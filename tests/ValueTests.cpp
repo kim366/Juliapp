198bbcfd3321b000712622251d09abc437ba6ceb
Kim <login@schmider.kim>
2018-11-03 08:07:05 +0100

    Add tests for mutable/immutable value references

--- tests/ValueTests.cpp
+++ tests/ValueTests.cpp
@@ -14,7 +14,6 @@ TEST_CASE("Boxing of primitive types")
 
 TEST_CASE("Moving variables into values")
 {
-
     struct NoCopy
     {
         NoCopy(const NoCopy&) = delete;
@@ -27,6 +26,7 @@ TEST_CASE("Moving variables into values")
     };
 
     jl::sync(jl::type<NoCopy>{"struct NoCopy x::Float32 end"});
+
     auto x = NoCopy{3.14f};
 
     REQUIRE(jl::value { std::move(x) }->x == 3.14f);
@@ -49,4 +49,39 @@ TEST_CASE("Literals")
     jl::eval("module SomeMod nested(x) = 3x end; free(x) = 2x");
     REQUIRE("SomeMod"_jlm == jl::module{"SomeMod"});
     REQUIRE("free"_jlf == jl::function{"free"});
+}
+
+TEST_CASE("References to mutable/immutable structs")
+{
+    jl::eval(R"(
+      mutable struct Mut x::Int64 end
+      struct Immut x::Int64 end
+    )");
+
+    struct Mut
+    {
+        int64_t x;
+        Mut& operator=(const Mut&) = default;
+    };
+
+    struct Immut
+    {
+        int64_t x; // should be const in practice
+        Immut&
+            operator=(const Immut&) = default; // should be delete in practice
+    };
+
+    jl::sync(jl::type<Mut>{"Mut"}, jl::type<Immut>{"Immut"});
+    auto mut = jl::value<Mut>{{3}};
+    auto immut = jl::value<const Immut>{{7}};
+    auto nonconst_immut = jl::value<Immut>{{9}};
+
+    REQUIRE((*mut).x == 3);
+    mut->x = 5;
+    REQUIRE((*mut).x == 5);
+    REQUIRE(mut->x == 5);
+
+    CHECK_NOTHROW(jlpp_static_assert(std::is_const_v<decltype(*immut)>));
+    CHECK_THROWS_AS(nonconst_immut->x = 5, jl::test::failed_assertion);
+    CHECK_NOTHROW(nonconst_immut->x == 3);
 }
