2d976627cba59f460e0527eb4de051ff9e73c4df
Kim Schmider <hello@schmider.kim>
2019-04-07 17:02:27 +0200

    Exapand call tests

--- tests/FreeFunctionTests.cpp
+++ tests/FreeFunctionTests.cpp
@@ -7,6 +7,8 @@ TEST_CASE("Calling Functions with free function")
 {
     jl::eval("f(x) = 3x; g(x::Int32) = 2x");
     REQUIRE(jl::call("f", 5).get<int>() == 15);
+    REQUIRE(jl::call("f", 3.).get<double>() == 9.);
+    REQUIRE(jl::call("f", 3.).get<float>() == 9.f);
     CHECK_THROWS_AS(jl::call("g", 5ULL), jl::language_error);
     CHECK_THROWS_AS(jl::call("h", 13.), jl::language_error);
 }
