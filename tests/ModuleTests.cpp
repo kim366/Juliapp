33722bc58b1beb3d881acc16f4d66c3031369eec
Kim Schmider <hello@schmider.kim>
2019-04-14 09:34:55 +0200

    Add conversion to module test

--- tests/ModuleTests.cpp
+++ tests/ModuleTests.cpp
@@ -8,6 +8,9 @@ TEST_CASE("Function returning Module")
     jl::eval(
         "module ReturnedModule f() = 3 end; returning_module() = ReturnedModule");
     auto m = jl::call("returning_module").get_module();
+    jl::module m2 = jl::call("returning_module");
     auto f = jl::function{m, "f"};
+    auto f2 = jl::function{m2, "f"};
     CHECK(f().get<int>() == 3);
+    CHECK(f2().get<int>() == 3);
 }
