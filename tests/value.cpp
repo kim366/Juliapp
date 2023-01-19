e93957a8fea31664fe25918355922bc5c055ac45
Kim Schmider <hello@schmider.kim>
2023-01-19 15:11:02 +1100

    Test copy/move assignment

--- tests/value.cpp
+++ tests/value.cpp
@@ -7,16 +7,16 @@ TEST_CASE("Value")
 {
     // before each test
     const auto NUM_BACKGROUND_ROOTED = num_rooted();
+    auto* DUMMY = jl_nothing;
     auto* INPUT = jl_box_uint8(19);
 
     SECTION("Wrapping value gives access to raw value")
     {
         auto* expected = INPUT;
+
         const auto subject = jl::value{jl::from_raw, INPUT};
 
-        auto* result = subject.raw();
-
-        REQUIRE(result == expected);
+        REQUIRE(subject.raw() == expected);
     }
 
     SECTION("Wrapping value roots it")
@@ -38,29 +38,52 @@ TEST_CASE("Value")
     SECTION("Copying wrapped value keeps underlying raw value")
     {
         auto* expected = INPUT;
-        const auto subject = jl::value{jl::from_raw, INPUT};
+        const auto wrapped = jl::value{jl::from_raw, INPUT};
 
-        const auto copy = subject; // NOLINT(performance-unnecessary-copy-initialization)
+        const auto subject = wrapped; // NOLINT(performance-unnecessary-copy-initialization)
 
-        REQUIRE(copy.raw() == expected);
+        REQUIRE(subject.raw() == expected);
     }
 
     SECTION("Moving wrapped value keeps underlying raw value")
     {
         auto* expected = INPUT;
-        auto subject = jl::value{jl::from_raw, INPUT};
+        auto wrapped = jl::value{jl::from_raw, INPUT};
 
-        const auto copy = std::move(subject);
+        const auto subject = std::move(wrapped);
 
-        REQUIRE(copy.raw() == expected);
+        REQUIRE(subject.raw() == expected);
+    }
+
+    SECTION("Wrapped copy-assignment updates underlying raw value")
+    {
+        auto* expected = INPUT;
+        auto subject = jl::value{jl::from_raw, DUMMY};
+        const auto wrapped = jl::value{jl::from_raw, INPUT};
+
+        subject = wrapped; // NOLINT(performance-unnecessary-copy-initialization)
+
+        REQUIRE(subject.raw() == expected);
+    }
+
+    SECTION("Wrapped move-assignment updates underlying raw value")
+    {
+        auto* expected = INPUT;
+        auto subject = jl::value{jl::from_raw, DUMMY};
+        auto wrapped = jl::value{jl::from_raw, INPUT};
+
+        subject = std::move(wrapped);
+
+        REQUIRE(subject.raw() == expected);
     }
 
     SECTION("Copying wrapped value roots new value")
     {
-        const auto subject = jl::value{jl::from_raw, INPUT};
+        const auto wrapped = jl::value{jl::from_raw, INPUT};
 
         {
-            const auto copy = subject; // NOLINT(performance-unnecessary-copy-initialization)
+            const auto subject = wrapped; // NOLINT(performance-unnecessary-copy-initialization)
+            UNUSED(subject);
 
             REQUIRE(num_rooted() == NUM_BACKGROUND_ROOTED + 2);
         }
@@ -70,14 +93,44 @@ TEST_CASE("Value")
 
     SECTION("Moving wrapped does not re-root")
     {
-        auto subject = jl::value{jl::from_raw, INPUT};
+        auto wrapped = jl::value{jl::from_raw, INPUT};
 
         {
-            const auto copy = std::move(subject);
+            const auto subject = std::move(wrapped);
+            UNUSED(subject);
 
             REQUIRE(num_rooted() == NUM_BACKGROUND_ROOTED + 1);
         }
 
         REQUIRE(num_rooted() == NUM_BACKGROUND_ROOTED + 0);
+    }
+
+    SECTION("Wrapped value copy-assignment frees old and roots new")
+    {
+        auto subject = jl::value{jl::from_raw, DUMMY};
+        const auto wrapped = jl::value{jl::from_raw, INPUT};
+
+        subject = wrapped;
+        UNUSED(subject);
+
+        REQUIRE(num_rooted(DUMMY) == 0);
+        REQUIRE(num_rooted(INPUT) == 2);
+    }
+
+    SECTION("Wrapped value move-assignment frees old and does not re-root")
+    {
+        auto subject = jl::value{jl::from_raw, DUMMY};
+
+        // create separate scope because `DUMMY` is only destroyed
+        // when `other` goes out of scope
+        {
+            auto wrapped = jl::value{jl::from_raw, INPUT};
+
+            subject = std::move(wrapped);
+            UNUSED(subject);
+        }
+
+        REQUIRE(num_rooted(DUMMY) == 0);
+        REQUIRE(num_rooted(INPUT) == 1);
     }
 }
