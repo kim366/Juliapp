9784ddcac17d430f26f911a01b91938bf53c797d
Kim Schmider <hello@schmider.kim>
2023-01-19 21:11:28 +1100

    Fix exception semantics

--- src/errors.hpp
+++ src/errors.hpp
@@ -17,28 +17,12 @@ inline std::string datatype_name(jl_value_t* type)
 
 } // namespace impl
 
-struct error : std::runtime_error
-{
-protected:
-    using std::runtime_error::runtime_error;
-};
-
-struct language_error : error
-{
-    using error::error;
-};
-
-struct bad_value_cast : error
+struct bad_value_cast : std::logic_error
 {
     bad_value_cast(const char* expected_type, jl_value_t* val)
-        : error{std::string{"Expected "} + expected_type + " but value contains " + impl::datatype_name(jl_typeof(val))}
+        : std::logic_error{std::string{"Expected "} + expected_type + " but value contains " + impl::datatype_name(jl_typeof(val))}
     {
     }
-};
-
-struct load_error : error
-{
-    using error::error;
 };
 
 } // namespace jl
