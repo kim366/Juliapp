15c0f8ec38723a04bc4e138bf70520e90ad01897
Kim Schmider <hello@schmider.kim>
2023-01-22 00:46:59 +1100

    Use lcov

--- src/errors.hpp
+++ src/errors.hpp
@@ -10,19 +10,13 @@ namespace jl
 namespace impl
 {
 
-inline std::string datatype_name(jl_value_t* type)
-{
-    return jl_string_ptr(jl_call1(impl::repr_fn, type));
-}
+std::string datatype_name(jl_value_t* type);
 
 } // namespace impl
 
 struct bad_value_cast : std::logic_error
 {
-    bad_value_cast(const char* expected_type, jl_value_t* val)
-        : std::logic_error{std::string{"Expected "} + expected_type + " but value contains " + impl::datatype_name(jl_typeof(val))}
-    {
-    }
+    bad_value_cast(const char* expected_type, jl_value_t* val);
 };
 
 } // namespace jl
