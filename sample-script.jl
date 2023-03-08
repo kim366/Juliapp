b4b1314e7bef3fc11173997fa20b6ae81c49c2f1
Kim Schmider <hello@schmider.kim>
2023-03-08 21:51:09 +1100

    Remove unused

--- sample-script.jl
+++
@@ -1,7 +0,0 @@
-pythagoras(x, y) = √(x^2 + y^2)
-
-println("----Starting Output from script!----")
-println("No recompiling necessary!")
-println(pythagoras(3, 4))
-println(pythagoras(5, 12))
-println("-----Ending Output from script!-----")
